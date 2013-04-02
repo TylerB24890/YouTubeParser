88f567482619a8a5b435d44e16935b16777d4d8f
Tyler Bailey <tyler@hirestarts.com>
2013-04-02 13:39:23 -0300

    Update class_youtube.php
    fixed the indentation issues

--- class_youtube.php
+++ class_youtube.php
@@ -2,12 +2,12 @@
 
 /*********************************************************
  *
- *  	@project   YouTube URL Parser
+ *  		@project   YouTube URL Parser
  *		@author    Tyler Bailey
  *		@version   1.0
  *
  *		@desc	   This class contains functions for 
- *				   parsing YouTube Video URLS and getting the video information
+ *			   parsing YouTube Video URLS and getting the video information
  *
  *********************************************************/
 
@@ -21,9 +21,9 @@ class YouTubeParser
     public $width     = 640; // video width
     public $height    = 360; // video height
 	
-	// empty __construct() function
-    function __construct() {
-    }
+ 	// empty __construct() function
+    	function __construct() {
+    	}
 	
 	/*************************************
 	 *
@@ -38,9 +38,9 @@ class YouTubeParser
 	 *			$youtube->init('height', 480);
 	 *
 	 *************************************/
-    function init($key,$val) {
-        return $this->$key = $val;
-    }
+   	 function init($key,$val) {
+      	  	return $this->$key = $val;
+    	}
 	
 	/*************************************
 	 *
@@ -52,48 +52,48 @@ class YouTubeParser
 	 *			$videoEmbed = $videoArray['0']['embed'];
 	 *
 	 *************************************/	
-    function getInfo() {
+    	function getInfo() {
 		// set the return array
-        $return = array();
+        	$return = array();
 		// check for secure domain
-        $domain = 'http'.($this->secure?'s':'').'://www.youtube'.($this->privacy?'-nocookie':'').'.com';
+        	$domain = 'http'.($this->secure?'s':'').'://www.youtube'.($this->privacy?'-nocookie':'').'.com';
 		// set the video height and size
-        $size   = 'width="'.$this->width.'" height="'.$this->height.'"';
+        	$size   = 'width="'.$this->width.'" height="'.$this->height.'"';
 		
 		// match the provided source URL to the regular expression
-        preg_match_all('/(youtu.be\/|\/watch\?v=|\/embed\/)([a-z0-9\-_]+)/i',$this->source,$matches);
+        	preg_match_all('/(youtu.be\/|\/watch\?v=|\/embed\/)([a-z0-9\-_]+)/i',$this->source,$matches);
 		
 		// if the source URL matches the regular expression
-        if(isset($matches[2])) {
+        	if(isset($matches[2])) {
 		
 			// if the unique setting is set to true
-            if($this->unique) {
-                $matches[2] = array_values(array_unique($matches[2])); // remove all duplicates
-            }
+            		if($this->unique) {
+                		$matches[2] = array_values(array_unique($matches[2])); // remove all duplicates
+            		}
 			
 			// foreach source URL that matched the regular expression
-            foreach($matches[2] as $key=>$id) {
+            		foreach($matches[2] as $key=>$id) {
 			
 				// get the video ID
-                $return[$key]['id']       = $id;
+                		$return[$key]['id']       = $id;
 				
 				// return the embed HTML
-                $return[$key]['embed']    = '<iframe '.$size.' src="'.$domain.'/embed/'.$id.($this->suggested?'':'?rel=0').'" frameborder="0" allowfullscreen></iframe>'; // 2011-2013 embed HTML code
+                		$return[$key]['embed']    = '<iframe '.$size.' src="'.$domain.'/embed/'.$id.($this->suggested?'':'?rel=0').'" frameborder="0" allowfullscreen></iframe>'; // 2011-2013 embed HTML code
 				
-                $return[$key]['embedold'] = '<object '.$size.'>
-                <param name="movie" value="'.$domain.'/v/'.$id.'?version=3'.($this->suggested?'':'&amp;rel=0').'"></param>
-                <param name="allowFullScreen" value="true"></param>
-                <param name="allowscriptaccess" value="always"></param>
-                <embed src="'.$domain.'/v/'.$id.'?version=3'.($this->suggested?'':'&amp;rel=0').'" type="application/x-shockwave-flash" '.$size.' allowscriptaccess="always" allowfullscreen="true"></embed>
-                </object>'; // 2006-2011 embed HTML code (for older systems/browsers)
+                		$return[$key]['embedold'] = '<object '.$size.'>
+                		<param name="movie" value="'.$domain.'/v/'.$id.'?version=3'.($this->suggested?'':'&amp;rel=0').'"></param>
+                		<param name="allowFullScreen" value="true"></param>
+                		<param name="allowscriptaccess" value="always"></param>
+                		<embed src="'.$domain.'/v/'.$id.'?version=3'.($this->suggested?'':'&amp;rel=0').'" type="application/x-shockwave-flash" '.$size.' allowscriptaccess="always" allowfullscreen="true"></embed>
+                		</object>'; // 2006-2011 embed HTML code (for older systems/browsers)
 				
 				// return the video thumbnails supplied by YouTube
-                $return[$key]['thumb']    = 'http://i4.ytimg.com/vi/'.$id.'/default.jpg'; // standard definition img
-                $return[$key]['hqthumb']  = 'http://i4.ytimg.com/vi/'.$id.'/hqdefault.jpg'; // high definition img
-            }
-        }
-        return $return;
-    }
+                		$return[$key]['thumb']    = 'http://i4.ytimg.com/vi/'.$id.'/default.jpg'; // standard definition img
+                		$return[$key]['hqthumb']  = 'http://i4.ytimg.com/vi/'.$id.'/hqdefault.jpg'; // high definition img
+            		}
+       		 }
+        	return $return;
+    	}
 }
 
 ?>
